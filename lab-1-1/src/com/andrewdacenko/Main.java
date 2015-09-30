14f038391e3bc9ed6c6e04534d6643d498499049
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-10-01 01:25:11 +0300

    add(Main): read file #3

---
+++ lab-1-1/src/com/andrewdacenko/Main.java
@@ -0,0 +1,24 @@
+package com.andrewdacenko;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            String file = readFile("triangles.json", StandardCharsets.UTF_8);
+            System.out.println(file);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+}
