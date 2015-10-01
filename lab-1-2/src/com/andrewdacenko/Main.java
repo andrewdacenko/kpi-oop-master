2518aec6f5e304222a141016dc227dce8132df86
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-10-01 04:15:35 +0300

    feature(Main) read list of files

--- lab-1-2/src/com/andrewdacenko/Main.java
+++ lab-1-2/src/com/andrewdacenko/Main.java
@@ -5,6 +5,20 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
+        String path = "./files";
 
+        String files;
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles)
+        {
+
+            if (file.isFile())
+            {
+                files = file.getName();
+                System.out.println(files);
+            }
+        }
     }
 }
