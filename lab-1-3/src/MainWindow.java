0e34d13a6d97096c779f3ed42ba5e4c1194089e1
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-12-24 05:47:14 +0200

    chore(imports) optimize

--- lab-1-3/src/MainWindow.java
+++ lab-1-3/src/MainWindow.java
@@ -1,9 +1,6 @@
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class MainWindow extends Frame implements ActionListener {
