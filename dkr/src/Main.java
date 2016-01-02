0673a37e1f350be54af2d770eaec2cbde3951ef4
Andrew Dacenko <a.s.dvornik@gmail.com>
2016-01-03 01:30:11 +0300

    refactor(DataGenerator) moved init into methods

--- dkr/src/Main.java
+++ dkr/src/Main.java
@@ -1,9 +1,13 @@
+import com.andrewdacenko.structures.DataGenerator;
+
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String args[]) {
         SwingUtilities.invokeLater(() -> {
+            DataGenerator.initialize();
+
             MainForm main = new MainForm();
             main.setVisible(true);
         });
